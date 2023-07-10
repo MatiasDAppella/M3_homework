7b89609f27ef489b2352229d2aa647338c77bc0b
MatiasDAppella <appellamatias@gmail.com>
2023-07-10 15:55:16 -0300

    01 done!

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,9 +2,24 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function print(output){
+    process.stdout.write(output)
+    process.stdout.write("\nprompt > ")
+}
 
+function bash(){
+    process.stdout.write("prompt > ")
+    process.stdin.on("data", (data) => {
+        let args = data.toString().split(" ").slice(1).join(" ").trim()
+        let cmd = data.split(" ")[0]
+
+        if (commands.hasOwnProperty(cmd)) commands[cmd](print, args);
+        else print(`command not found: ${cmd}`)
+    })
+}
+    
 bash();
+
 module.exports = {
    print,
    bash,
